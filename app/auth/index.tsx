cfd6cd6127a562ec1a40db888431ae6e2ae13421
Fazlul Karim Shahed <fazlul.karim.shahed@gmail.com>
2026-08-16 19:12:08 +0600

    feat(auth): restore Google Sign-In button in Expo Go / development client

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -14,8 +14,6 @@ import { Button } from '../../src/components/ui/Button';
 import { Spacing, Radii, Typography, useThemeColors } from '../../src/constants/colors';
 import { AuthService, FirebaseService, auth } from '../../src/services/firebase';
 import { useTransactionStore, useUIStore } from '../../src/store';
-import Constants from 'expo-constants';
-
 
 WebBrowser.maybeCompleteAuthSession();
 
